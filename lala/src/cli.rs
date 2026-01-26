f44b599dbf98ec7e7f1b900d35d80aae978ed281
dipghoshraj <dipghoshraj@gmail.com>
2026-01-27 00:43:37 +0530

    stream the tokens

--- lala/src/cli.rs
+++ lala/src/cli.rs
@@ -1,9 +1,13 @@
 use std::io::{self, Write};
 use crate::agent::model::ModelWrapper;
+use llama_cpp::standard_sampler::StandardSampler;
+use llama_cpp::{LlamaModel, LlamaParams, SessionParams};
+
 
 pub fn run(model_path: &str) -> anyhow::Result<()> {
     let model = ModelWrapper::load(model_path)?;
     let mut session = model.create_session()?;
+
 
     println!("🦙 llama-agent ready (/exit to quit)");
 
@@ -18,8 +22,20 @@ pub fn run(model_path: &str) -> anyhow::Result<()> {
             break;
         }
 
-        let response = session.complete(input, 512)?;
-        println!("{}", response);
+        let prompt = "User: Hello, who are you?\nAssistant:";
+        session.session.advance_context(prompt)?;
+
+        // let response = session.complete(input, 512)?;
+        let mut stream = session.session.start_completing_with(StandardSampler::default(), 512)?;
+        println!("--- response ---");
+        while let Some(token) = stream.next_token() {
+
+
+            let text = model.model.token_to_piece(token);
+            print!("{:?}", text);
+            io::stdout().flush().unwrap();
+        }
+        println!();
     }
 
     Ok(())
